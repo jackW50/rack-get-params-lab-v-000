868977ebcfa38ba2d64172ffba149a006e599215
John Werenski <jtwerenski@gmail.com>
2018-11-20 17:38:27 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -14,6 +14,9 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    elsif req.path.match(/add/)
+      search_term = req.params["q"]
+      add_to_cart
     elsif req.path.match(/cart/)
       @@cart.each do |item|
         resp.write "#{item}"
